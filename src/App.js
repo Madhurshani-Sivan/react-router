7d83eb18973914006def5eb04cf9225e3f8f1a6e
madhurshani-sivan <madhurshanisivan@gmail.com>
2023-08-07 08:56:28 +0530

    add route to Products Page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/Home";
+import ProductsPage from "./pages/Products";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
+  },
+  {
+    path: "/products",
+    element: <ProductsPage />,
   },
 ]);
 
