b944e709efa4ac4cfd7489c0a2f5d12057659110
madhurshani-sivan <madhurshanisivan@gmail.com>
2023-08-08 12:36:32 +0530

    add index routes

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       {
-        path: "",
+        index: true, //ALTERNATIVE FOR path:''
         element: <HomePage />,
       },
       {
